c3c84d876a730ac8d9625d9032c0ebd0a71aca8b
Daniil-Svetlov <Rushjs@yandex.ru>
2026-07-27 23:12:50 +0300

    Update urls.py

--- django-api/config/urls.py
+++ django-api/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
+        path('admin/', csrf_exempt(admin.site.urls)),
     path("admin/", admin.site.urls),
     path("api/", include("trading.urls")),
 ]
